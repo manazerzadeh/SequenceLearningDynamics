8687b0b57a99f01ed2413eacba58e9878cd5068b
Jörn Diedrichsen <joern.diedrichsen@googlemail.com>
2025-04-20 22:32:13 -0400

    Model fitting

--- model.py
+++ model.py
@@ -1,33 +1,55 @@
 import numpy as np
+from scipy.optimize import minimize
 
 class LdModel():
     def __init__(self):
-        self.Y0 = 2500
-        self.fast_tau = 1
+        self.y0 = 2.2
+        self.fast_tau = 0.01
         self.fast_B = 200
-        self.slow_amp = 1300
-        self.slow_B = 20
+        self.slow_amp = 3
+        self.slow_B = 2
         self.slow_A = self.slow_amp/(self.slow_B+self.slow_amp)
         self.fatigue_tau = 0.3
-        self.fatigue_B = 10
-        self.param_list = ['fast_A','fast_tau','slow_amp','slow_tau','fatigue_tau','fatigue_A']
+        self.fatigue_B = 0
+        self.param_list = ['y0','fast_tau','slow_amp','slow_tau','fatigue_tau','fatigue_A']
+
+    def update_params(self):
+        self.fast_A = self.fast_amp/(self.fast_B+self.fast_amp)
+        self.fast_tau = -np.log(self.fast_A )
+        self.slow_A = self.slow_amp/(self.slow_B+self.slow_amp)
 
     def set_params(self,theta):
         self.nparams = len(self.param_list)
         if theta.shape[0]!=self.nparams:
             raise(NameError('theta not the right size'))
         for i,l in enumerate(self.param_list):
-            self[l]=theta[i]
+            setattr(self,l,np.exp(theta[i]))
+        self.update_params()
         return self
 
     def get_params(self):
         self.nparams = len(self.param_list)
         theta = np.zeros(self.nparams)
-        for i,l in enumerate(param_list):
-            theta[i] = self[l]
+        for i,l in enumerate(self.param_list):
+            theta[i] = np.log(getattr(self,l))
         return theta
 
 class LdModel_additive(LdModel):
+    def __init__(self):
+        self.y0 = 2
+        self.fast_amp = 1.5
+        self.fast_B = 0.04
+        self.slow_amp = 3
+        self.slow_B = 0.01
+        self.fatigue_tau = 0.3
+        self.fatigue_B = 0
+        self.update_params()
+        self.param_list = ['y0','fast_amp','fast_B','slow_amp','slow_B']
+
+    def update_params(self):
+        self.fast_A = self.fast_amp/(self.fast_B+self.fast_amp)
+        self.fast_tau = -np.log(self.fast_A )
+        self.slow_A = self.slow_amp/(self.slow_B+self.slow_amp)
 
     def predict(self,time,seq=None):
         N = time.shape[0]
@@ -36,7 +58,7 @@ class LdModel_additive(LdModel):
         z_fatigue = np.zeros((N))
         yp = np.zeros((N))
         for i,t in enumerate(time):
-            yp[i]=self.Y0 - z_fast[i] - z_slow[i]  + z_fatigue[i]
+            yp[i]=self.y0 + z_fast[i] + z_slow[i] - z_fatigue[i]
             if i < N-1:
                 dt = time[i+1]-time[i]
                 fast_A =np.exp(-self.fast_tau*dt)
@@ -46,22 +68,20 @@ class LdModel_additive(LdModel):
                 z_fatigue[i+1] =  fatigue_A * (z_fatigue[i] +  self.fatigue_B)
         return yp, z_fast , z_slow,z_fatigue
 
+def L2_loss(theta,M,D):
+    """
+    Computes the L2 loss
+    """
+    M.set_params(theta)
+    yp,z_fast,z_slow,z_fatigue = M.predict(D.time)
+    loss = np.sum((D.speed-yp)**2)
+    return loss
 
-class LdModel_additive(LdModel):
-
-    def predict(self,time,seq=None):
-        N = time.shape[0]
-        z_fast = np.zeros((N))
-        z_slow = np.zeros((N))
-        z_fatigue = np.zeros((N))
-        yp = np.zeros((N))
-        for i,t in enumerate(time):
-            yp[i]=self.Y0 - z_fast[i] - z_slow[i]  + z_fatigue[i]
-            if i < N-1:
-                dt = time[i+1]-time[i]
-                fast_A =np.exp(-self.fast_tau*dt)
-                z_fast[i+1] =  fast_A * (z_fast[i] +  self.fast_B)
-                z_slow[i+1] = self.slow_A * (z_slow[i] + self.slow_B)
-                fatigue_A =np.exp(-self.fatigue_tau*dt)
-                z_fatigue[i+1] =  fatigue_A * (z_fatigue[i] +  self.fatigue_B)
-        return yp, z_fast , z_slow,z_fatigue
+def fit_model(D, M):
+    """
+    Fits the model to the data
+    """
+    theta0=M.get_params()
+    res = minimize(L2_loss, theta0, args=(M,D), method='L-BFGS-B')
+    M=M.set_params(res.x)
+    return M
--- test_model.py
+++ test_model.py
@@ -1,10 +1,19 @@
 import SequenceLearningDynamics.model as mo
+import SequenceLearningDynamics.utils as ut
+import SequenceLearningDynamics.setglobals as gl
 import numpy as np
+import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 
 
-if __name__=='__main__':
+n_trials_per_block = 20
+n_trials_per_day = 400
+n_blocks_per_day = 20
+n_days = 3
+
+
+def test_model():
     M = mo.LdModel_additive()
     time = np.concatenate([np.linspace(0,100,100),np.linspace(150,250,100),np.linspace(300,400,100)],axis=0)
     trial = np.arange(np.shape(time)[0])
@@ -17,4 +26,74 @@ if __name__=='__main__':
     plt.plot(time,z_slow,'r.')
     plt.subplot(4,1,4)
     plt.plot(time,z_fatigue,'r.')
+    pass
+
+
+def get_data(subj) -> pd.DataFrame:
+    """
+    Gets the average data for a subject
+    """
+    D=pd.read_csv(gl.result_dir + 'SL3_all_trial_data.tsv', delimiter='\t')
+    D['error'] = (D.seqError>0).astype(int)
+    D['speed'] = 1/D.MT*1000*5
+
+    if subj == 'all':
+        D = D[D.error==0].groupby(['day','BN','TN']).agg({'speed':'mean','MT':'median','error':'min'}).reset_index()
+    else:
+        D = D[D.SubNum==subj]
+        D[D.error==1].speed = np.nan
+        D[D.MT==1].speed = np.nan
+    D['tn'] = np.arange(D.shape[0])+1
+    D['BN'] = D['BN'] -6
+    D['BN_in_day'] = (D.BN  - (D.day-1)*n_blocks_per_day)
+    D['time'] = D['day']*24*60*60/4 + (D['BN_in_day']-1)*35 + (D['TN']-1)
+    return D
+
+def plot_data(D):
+    """
+    Plots the data
+    """
+    plt.plot(D.tn, D.speed, 'k.')
+    block = np.where(D.TN==2)[0]+1
+    dblock = np.where((D.BN_in_day==1) & (D.TN==2))[0]+1
+    for i in block:
+        plt.axvline(x=i, color=(0.8,0.8,0.8), linestyle=':')
+    for i in dblock:
+        plt.axvline(x=i, color='k', linestyle='-')
+    if 'yp' in D.columns:
+        plt.plot(D.tn,D.yp,':')
+    if 'z_slow' in D.columns:
+        plt.plot(D.tn,D.z_slow,'b:')
+
+def plot_block(data):
+    D = data[data.error==0].groupby(['day','BN','BN_in_day']).agg({'speed':'mean','MT':'median','yp':'mean','z_slow':'mean'}).reset_index()
+    plt.plot(D.BN, D.speed, 'k.')
+    dblock = np.where((D.BN_in_day==1))[0]+0.5
+    for i in dblock:
+        plt.axvline(x=i, color='k', linestyle='-')
+    plt.plot(D.BN,D.yp,':')
+    plt.plot(D.BN,D.z_slow,'b:')
+
+def plot_trial(D):
+    D['BN_in_day'] = (D.BN  - (D.day-1)*n_blocks_per_day - 6)
+    T=D[(D.error==0) & (D.day>1)]
+    Dtrial = T.groupby(['TN']).agg({'speed':'mean','MT':'median','yp':'mean','z_slow':'mean'}).reset_index()
+    plt.plot(Dtrial.TN, Dtrial.speed, 'k.')
+    plt.plot(Dtrial.TN,Dtrial.yp,':')
+
+if __name__=='__main__':
+    D= get_data('all')
+    M = mo.LdModel_additive()
+    M = mo.fit_model(D,M)
+    yp,z_fast,z_slow,z_fatigue = M.predict(D.time)
+    z_slow = M.y0 + z_slow
+    D['yp'] = yp
+    D['z_slow'] = z_slow
+    plt.figure(1)
+    plot_data(D)
+    plt.figure(2)
+    plot_block(D)
+    plt.figure(3)
+    plot_trial(D)
+
     pass
